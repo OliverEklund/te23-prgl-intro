f5b10b2068f602bd36c868be2631f85c79117182
OliverEklund <oliver.eklund@elev.ga.ntig.se>
2024-09-12 09:34:04 +0200

    Finished Kung.py

--- Kung.py
+++ Kung.py
@@ -1,6 +1,7 @@
 from random import randint
 
-print("Hej och välkommen till tärnings spelet, två spelare kommer slå varsinn tärning, först till tre vinster vinner spelet")
+print("Hej och välkommen till tärnings spelet, två spelare kommer slå varsinn tärning.")
+print("Först till fem vinster vinner spelet")
 player_one_name = input("Spelare Ett, skriv ditt namn: ")
 player_two_name = input("spelare Två, skriv ditt namn: ")
 
@@ -14,13 +15,23 @@ while play_game == "J":
     player_two_roll = randint(1, 6)
     
     if player_one_roll > player_two_roll:
-        print(f"{player_one_name} ett vinner med slaget {player_one_roll}. {player_two_name} slog {player_two_roll}")
+        print(f"{player_one_name} vinner med slaget {player_one_roll}. {player_two_name} slog {player_two_roll}")
         player_one_score += 1
    
     elif player_two_roll > player_one_roll:
-        print(f"{player_two_name} två vinner med slaget {player_two_roll}. {player_one_name} slog {player_one_roll}")
+        print(f"{player_two_name} vinner med slaget {player_two_roll}. {player_one_name} slog {player_one_roll}")
         player_two_score += 1
 
     else:
         print(f"Ingen spelare vinner, det är oavgjort, båda slog {player_one_roll}")
-    play_game = input("Vill du spela igen(J/N): ")
+
+    if player_one_score == 5:
+        print(f"{player_one_name} vann hela spelet genom att få fem vinster först!")
+        play_game = "N"
+    
+    if player_two_score ==5:
+        print(f"{player_two_name} vann hela spelet genom att få fem vinster först!")
+        play_game = "N"
+    
+    else:
+         play_game = input("Vill du spela igen(J/N): ")
